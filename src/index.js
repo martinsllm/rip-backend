88cc4148ee0a133c00b6b8abd34b58a429c9cf24
lucas <lucaslm2010@gmail.com>
2022-01-18 19:26:35 -0300

    Utilizando o morgan para verificaçao dos logs de requisiçoes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const morgan = require('morgan');
 
 const express = require('express');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(morgan('dev'));
 app.use(require('./routes/routes'));
 
 app.listen(process.env.PORT || 8080);
