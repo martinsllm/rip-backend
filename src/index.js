2b3cc742bbd1cc6dbd5e86370596bf507392ea5e
lucas <lucaslm2010@gmail.com>
2022-01-28 18:51:23 -0300

    Renomeando o arquivo de middleware para exceptions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const morgan = require('morgan');
-const middleware = require('./middlewares/middleware')
 
 const express = require('express');
 
@@ -9,7 +8,7 @@ const app = express();
 app.use(express.json({limit: '10MB'}));
 app.use(morgan('dev'));
 app.use(require('./routes/routes'));
-app.use(middleware);
+app.use(require('./middlewares/exceptions'));
 
 
 app.listen(process.env.PORT || 8080);
