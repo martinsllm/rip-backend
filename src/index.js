c6b564ed3063ec1a53ea1290293fa7096b98e720
lucas <lucaslm2010@gmail.com>
2022-01-29 11:13:46 -0300

    Configurando cors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const express = require('express');
 const app = express();
 
 app.use(express.json({limit: '10MB'}));
+
+app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	
+	next();
+});
+
 app.use(morgan('dev'));
 app.use(require('./routes/routes'));
 app.use(require('./middlewares/exceptions'));
