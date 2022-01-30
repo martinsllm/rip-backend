56d3975a721aa11fdd744905075159178e0c4e18
lucas <lucaslm2010@gmail.com>
2022-01-30 17:29:15 -0300

    Correçao nas rotas de categoria

--- src/routes/routes/category.js
+++ src/routes/routes/category.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const auth = require('../../middlewares/auth');
 const CategoryController = require('../../controllers/CategoryController');
 
 router.get('/', CategoryController.List);
